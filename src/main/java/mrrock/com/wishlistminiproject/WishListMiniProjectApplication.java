3916411cb4597d314a7862de4ef9395c009b7981
gustavtjac <gustavtorup@gmail.com>
2025-03-24 10:41:02 +0100

    init commit

---
+++ src/main/java/mrrock/com/wishlistminiproject/WishListMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package mrrock.com.wishlistminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishListMiniProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mrrock/com/wishlistminiproject/WishListMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mrrock.com.wishlistminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishListMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
