a063066a0f786e12ae48d52ade5e7a260d5b8aca
gustavtjac <gustavtorup@gmail.com>
2025-03-26 15:30:43 +0100

    forside fixet en smule

--- src/main/java/mrrock/com/wishlistminiproject/Models/Wishlist.java
+++ src/main/java/mrrock/com/wishlistminiproject/Models/Wishlist.java
@@ -3,11 +3,43 @@ package mrrock.com.wishlistminiproject.Models;
 import java.util.List;
 
 public class Wishlist {
+    private int id;
 private String name;
 private int userID;
 private List<Wish> wishList;
 
+    public Wishlist() {
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public List<Wish> getWishList() {
+        return wishList;
+    }
+
+    public void setWishList(List<Wish> wishList) {
+        this.wishList = wishList;
+    }
 }
