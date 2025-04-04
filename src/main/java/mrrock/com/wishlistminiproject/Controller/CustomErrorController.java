62292fb9bcaffdcf2bccfaca637cea0d0ca9026e
gustavtjac <gustavtorup@gmail.com>
2025-04-04 09:02:03 +0200

    error/500

--- src/main/java/mrrock/com/wishlistminiproject/Controller/CustomErrorController.java
+++ src/main/java/mrrock/com/wishlistminiproject/Controller/CustomErrorController.java
@@ -2,21 +2,27 @@ package mrrock.com.wishlistminiproject.Controller;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
+import mrrock.com.wishlistminiproject.Models.User;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
     @GetMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, HttpSession session, Model model) {
         String errorPage = "error"; // default
 
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         if (status != null) {
+            model.addAttribute("user",((User) session.getAttribute("user")));
+            model.addAttribute("loggedIn",((User) session.getAttribute("user"))!=null);
+
             Integer statusCode = Integer.valueOf(status.toString());
 
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
