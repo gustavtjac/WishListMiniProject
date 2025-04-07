fd2f21fadb0ca822f01336cf8fb01af7985a4d16
gustavtjac <gustavtorup@gmail.com>
2025-04-07 10:56:21 +0200

    lavet to unit tests

---
+++ src/test/java/mrrock/com/wishlistminiproject/Service/UserServiceTest.java
@@ -0,0 +1,40 @@
+package mrrock.com.wishlistminiproject.Service;
+
+import mrrock.com.wishlistminiproject.Models.User;
+import mrrock.com.wishlistminiproject.Repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class UserServiceTest {
+
+
+    @Test
+    void getUserByUsername_ReturnsUser(){
+
+        UserRepository mockRepo = mock(UserRepository.class);
+        UserService userService = new UserService(mockRepo);
+        User testUser = new User();
+        testUser.setUsername("findMyUser");
+        testUser.setName("testName");
+        testUser.setPassword("123");
+
+        when(mockRepo.findById("findMyUser")).thenReturn(testUser);
+
+
+        User result = userService.getUserFromUsername("findMyUser");
+        assertEquals(testUser.getUsername(), result.getUsername());
+        assertEquals(testUser.getName(), result.getName());
+        assertEquals(testUser.getId(), result.getId());
+        verify(mockRepo,times(1)).findById(testUser.getUsername());
+    }
+
+
+
+}
--- src/test/java/mrrock/com/wishlistminiproject/WishListMiniProjectApplicationTests.java
+++ src/test/java/mrrock/com/wishlistminiproject/WishListMiniProjectApplicationTests.java
@@ -1,13 +1,11 @@
 package mrrock.com.wishlistminiproject;
 
+import mrrock.com.wishlistminiproject.Service.UserServiceTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WishListMiniProjectApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
